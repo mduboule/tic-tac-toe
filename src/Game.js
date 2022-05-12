1b74a3e5e0f65933c623cae2add5fa7661e8491c
Marius Duboule <dev@mariusduboule.com>
2022-05-11 21:50:46 -0400

    Add victory modal and fix end game logic

--- src/Game.js
+++ src/Game.js
@@ -3,18 +3,27 @@ import Cell from './Cell'
 
 export default function Game() {
   const initialGame = {
-    status: "new", // "new" | "in progress" | "over"
-    winner: "", // "X" | "O" | ""
+    status: 'new', // "new" | "in progress" | "over"
+    winner: 0, // 0 = no winner | 1 = X is winner | 2 = O is winner
     round: 0, // maximum of 9
     grid: [[0, 0, 0], [0, 0, 0], [0, 0, 0]] // 0 = empty, 1 = X, 2 = O
   }
 
   const [game, setGame] = useState(initialGame)
+  const [finalMessage, setFinalMessage] = useState('')
 
-  // check for win
-  checkForWin() && setWinner(checkForWin())
+  // check for draw
+  if (game.round == 9 && game.winner === 0 && game.status !== 'over') {
+    closeGame(0)
+  }
+  else {
+    // check for win
+    checkForWin() && closeGame(checkForWin())
+  }
 
   function handleChange(x, y) {
+    // return if the game is over
+    if (game.status === 'over') return
     // return if the cell isn't empty
     if (game.grid[x][y] !== 0) return
 
@@ -25,7 +34,8 @@ export default function Game() {
       return {
         ...prevGame,
         grid: newGrid,
-        round: prevGame.round + 1
+        round: prevGame.round + 1,
+        status: 'in progress'
       }
     })
   }
@@ -78,7 +88,16 @@ export default function Game() {
     return checkGrid()
   }
 
-  function setWinner(winner) {
+  function closeGame(winner) {
+    // Game is a draw
+    if (winner === 0) {
+      setFinalMessage('Aww nobody won…')
+    } 
+    // Game is a win
+    else {
+      setFinalMessage(`Victory! Player ${winner === 1 ? 'X' : 'O'} for the win :)`)
+    }
+    if (game.status === 'over') return
     setGame(prevGame => (
       {
         ...prevGame,
@@ -107,11 +126,18 @@ export default function Game() {
 
   return (
     <>
-      <button 
-        onClick={() => setGame(initialGame)}
-        className="block m-auto mb-8 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-      >Start new game</button>
-      <div className="grid grid-cols-3 grid-rows-3 gap-1 bg-black">
+      {game.status === 'over' &&
+        <div className='absolute w-[33rem] p-8 rounded-xl border border-blue-800 bg-slate-200 drop-shadow-xl top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 h-auto'>
+          {game.status === 'over' && <h1 className='text-center text-5xl mb-8 leading-normal'>{finalMessage}</h1>}
+          <button 
+            onClick={() => setGame(initialGame)}
+            className={(game.status === 'over' ? 'block' : 'hidden') + ' m-auto mb-8 px-8 py-4 bg-blue-600 text-white font-medium text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'}
+          >
+            Start new game
+           </button>
+        </div>
+      }
+      <div className='grid grid-cols-3 grid-rows-3 gap-1 bg-black'>
         {gameElements}
       </div>
     </>
